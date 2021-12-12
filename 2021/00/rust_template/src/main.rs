caaaf942dc0d6a1dacc273a14fb079725a88518e
Simon Märtens <maertens@stce.rwth-aachen.de>
2021-12-12 17:53:08 +0100

    Updated template.

--- 2021/00/rust_template/src/main.rs
+++ 2021/00/rust_template/src/main.rs
@@ -1,5 +1,7 @@
-use std::fs;
 use std::env;
+
+// Import puzzle solutions module
+mod rust_template;
 
 // Main entry point
 fn main() {
@@ -9,44 +11,9 @@ fn main() {
   }
   let filename: &String = &args[1];
 
-  let val_1 = task_1(&filename);
+  let val_1 = rust_template::solution_1(&filename);
   println!("1. Solution: {}", val_1);
 
-  let val_2 = task_2(&filename);
+  let val_2 = rust_template::solution_2(&filename);
   println!("2. Solution: {}", val_2);
-}
-
-// First task
-fn task_1(filename: &String) -> i32 {
-  let contents = fs::read_to_string(filename)
-    .expect("Couldn't read input file.");
-  let _lines: Vec<&str> = contents.lines().collect();
-
-  return 0;
-}
-
-// Second task
-fn task_2(filename: &String) -> i32 {
-  let contents = fs::read_to_string(filename)
-    .expect("Couldn't read input file.");
-  let _lines: Vec<&str> = contents.lines().collect();
-
-  return 1;
-}
-
-// Test example inputs against the reference solution
-#[cfg(test)]
-mod tests {
-  use super::*;
-  const INPUT_FILENAME_1: &str = "input/example_input.txt";
-
-  #[test]
-  fn test_1() {
-    assert_eq!(task_1(&INPUT_FILENAME_1.to_string()), 0);
-  }
-
-  #[test]
-  fn test_2() {
-    assert_eq!(task_2(&INPUT_FILENAME_1.to_string()), 1);
-  }
 }
