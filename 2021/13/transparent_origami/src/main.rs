9ef2f4c4dd3a78c515ae727451583e08542a0456
Simon Maertens <maertens@stce.rwth-aachen.de>
2021-12-13 13:00:55 +0100

    Day 13.

---
+++ 2021/13/transparent_origami/src/main.rs
@@ -0,0 +1,25 @@
+extern crate clap;
+use clap::Parser;
+
+/// Day 13: Transparent Origami
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+struct Args {
+  /// Input file (e.g. input/puzzle_input.txt)
+  #[clap(short, long)]
+  filename: String
+}
+
+// Import puzzle solutions module
+mod transparent_origami;
+
+// Main entry point
+fn main() {
+  let args = Args::parse();
+
+  let n = transparent_origami::first_fold(&args.filename);
+  println!("1. Points after the first fold: {}", n);
+
+  let code = transparent_origami::gen_code(&args.filename);
+  print!("2. Code:\n{}", code);
+}
