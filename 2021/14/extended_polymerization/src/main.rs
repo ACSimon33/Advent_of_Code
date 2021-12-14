50c29083ea2f9e67ff9a6c054cf5fec77d4bca41
Simon Maertens <maertens@stce.rwth-aachen.de>
2021-12-14 12:43:08 +0100

    Day 14 part 1.

---
+++ 2021/14/extended_polymerization/src/main.rs
@@ -0,0 +1,28 @@
+extern crate clap;
+use clap::Parser;
+
+/// Day 0: Rust Template
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+struct Args {
+  /// Input file (e.g. input/puzzle_input.txt)
+  #[clap(short, long)]
+  filename: String,
+
+  /// Amount of of simulation steps.
+  #[clap(short, long, default_value_t = 1)]
+  steps: usize
+}
+
+// Import puzzle solutions module
+mod extended_polymerization;
+
+// Main entry point
+fn main() {
+  let args = Args::parse();
+
+  let occurences = extended_polymerization::get_elements(&args.filename, &args.steps);
+  println!("Occurences:\n{:?}", occurences);
+  println!("Max difference: {}",
+    occurences.values().max().unwrap() - occurences.values().min().unwrap());
+}
