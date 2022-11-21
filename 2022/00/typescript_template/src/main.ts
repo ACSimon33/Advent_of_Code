bc917e3976ff7033c4d15bbfae304ed4d9504927
Simon Maertens <simon@slmaertens.dev>
2022-11-21 10:55:52 +0100

    Added formatting with 'prettier'.

--- 2022/00/typescript_template/src/main.ts
+++ 2022/00/typescript_template/src/main.ts
@@ -11,17 +11,31 @@ interface AoCArgs {
 }
 
 // Args typed as AoCArgs
-export const args = parse<AoCArgs>({
-  filename: { type: String, alias: 'f', description: 'Input file (e.g. input/puzzle_input.txt)' },
-  help: { type: Boolean, optional: true, alias: 'h', description: 'Prints this usage guide' }
-}, {
-  helpArg: 'help',
-  headerContentSections: [{ header: 'Day 0', content: 'Typescript Template' }]
-});
+export const args = parse<AoCArgs>(
+  {
+    filename: {
+      type: String,
+      alias: 'f',
+      description: 'Input file (e.g. input/puzzle_input.txt)'
+    },
+    help: {
+      type: Boolean,
+      optional: true,
+      alias: 'h',
+      description: 'Prints this usage guide'
+    }
+  },
+  {
+    helpArg: 'help',
+    headerContentSections: [
+      { header: 'Day 00', content: 'Typescript Template' }
+    ]
+  }
+);
 
 // ************************************************************************** //
 
-import * as typescript_template from "./typescript_template";
+import * as typescript_template from './typescript_template';
 
 const val_1: number = typescript_template.solution_1(args.filename);
 console.log(val_1);
--- 2022/00/typescript_template/tests/typescript_template.test.ts
+++ 2022/00/typescript_template/tests/typescript_template.test.ts
@@ -1,11 +1,12 @@
 // Import puzzle solutions module
-import * as typescript_template from "../src/typescript_template";
+import * as typescript_template from '../src/typescript_template';
 
 // Example input
-const AoC22_root = process.env["AOC_ROOT"];
-const INPUT_FILENAME: string = (AoC22_root === undefined) ?
-  "./input/example_input.txt" :
-  AoC22_root + "/00/typescript_template/input/example_input.txt";
+const AoC22_root = process.env['AOC_ROOT'];
+const INPUT_FILENAME: string =
+  AoC22_root === undefined
+    ? './input/example_input.txt'
+    : AoC22_root + '/00/typescript_template/input/example_input.txt';
 
 // Register test suite
 describe('Testing typescript template project (day 0)', () => {
