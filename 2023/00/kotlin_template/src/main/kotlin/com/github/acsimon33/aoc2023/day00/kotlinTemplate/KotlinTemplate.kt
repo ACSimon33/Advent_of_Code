3dece3a878d5f2ef0032584581c5c395ae5aff7c
Simon Maertens <simon@slmaertens.dev>
2023-12-01 08:37:14 +0100

    Added more comments to day 00

--- 2023/00/kotlin_template/src/main/kotlin/com/github/acsimon33/aoc2023/day00/kotlinTemplate/KotlinTemplate.kt
+++ 2023/00/kotlin_template/src/main/kotlin/com/github/acsimon33/aoc2023/day00/kotlinTemplate/KotlinTemplate.kt
@@ -4,12 +4,12 @@ package com.github.acsimon33.aoc2023.day00.kotlinTemplate
 public class KotlinTemplate(input: String) {
     val lines: List<String> = input.lines()
 
-    /** First task */
+    /** First task: TODO */
     fun solution1(): Int {
         return 0
     }
 
-    /** Second task */
+    /** Second task: TODO */
     fun solution2(): Int {
         return 1
     }
