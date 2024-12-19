34a43b32d0d94efea6eab4ea040366d2cbe69262
Simon Maertens <simon@slmaertens.dev>
2024-12-19 07:32:33 +0100

    [2024] Add Day 19: Linen Layout

--- 2024/build.zig
+++ 2024/build.zig
@@ -63,4 +63,5 @@ pub fn build(b: *std.Build) void {
     add_subproject(b, target, optimize, test_step, benchmark_step, "16", "reindeer_maze");
     add_subproject(b, target, optimize, test_step, benchmark_step, "17", "chronospatial_computer");
     add_subproject(b, target, optimize, test_step, benchmark_step, "18", "ram_run");
+    add_subproject(b, target, optimize, test_step, benchmark_step, "19", "linen_layout");
 }
